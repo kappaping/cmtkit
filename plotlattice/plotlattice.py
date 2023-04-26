e3ec6dfe9b76c9c345629ad0a94c6bd80d393335
kappaping <peterepsiloncode@gmail.com>
2023-04-26 11:08:16 -0700

    4/26: Optimized plotting of the lattice

--- plotlattice/plotlattice.py
+++ plotlattice/plotlattice.py
@@ -36,11 +36,14 @@ def sites(rs,ltype,otype,os,res,to3d):
             sspl.mlab_source.dataset.point_data.scalars=os
         elif(otype=='s'):
             [s0,s1,s2]=np.array(os).transpose()
-            sspl=mlab.quiver3d(r0s,r1s,r2s,s0,s1,s2,colormap='coolwarm',mode='arrow',scale_factor=1.,resolution=res/2)
+            sspl=mlab.quiver3d(r0s,r1s,r2s,s0,s1,s2,colormap='coolwarm',mode='arrow',scale_factor=1.,resolution=res)
             sspl.glyph.color_mode='color_by_scalar'
             sspl.module_manager.scalar_lut_manager.use_default_range=False
             sspl.module_manager.scalar_lut_manager.data_range=[-1.,1.]
             sspl.mlab_source.dataset.point_data.scalars=s2
+            sspl.glyph.glyph_source.glyph_source.shaft_radius=0.05
+            sspl.glyph.glyph_source.glyph_source.tip_length=0.5
+            sspl.glyph.glyph_source.glyph_source.tip_radius=0.1
     else:
         if(otype=='l' or otype=='c'):
             plt.scatter(r0s,r1s,s=1.,c=os,cmap='coolwarm',vmin=-1.,vmax=1.)
@@ -58,23 +61,33 @@ def bonds(rs,Nall,ltype,otype,os,res):
     bc=0
     bs=[np.array([ltc.pos(pairt[0],ltype),ltc.pos(pairt[1],ltype)]).transpose() for r in rs for pairt in ltc.pairs(r,Nall[0][0],bc,ltype)[1]]
     if(otype=='l'):os=[np.array([0. for nb in range(len(bs))]),np.array([0. for nb in range(len(bs))])]
-    elif(otype=='c'):os=[np.array([(abs(ch)**0.75)*np.sign(ch) for ch in os[0]]),np.array([(abs(ch)**0.5)*np.sign(ch) for ch in os[1]])]
-    elif(otype=='s'):os=[np.array([(np.linalg.norm(sp)**0.75)*np.sign(sp[2]) for sp in os[0]]),np.array([(np.linalg.norm(sp)**0.5)*np.sign(sp[2]) for sp in os[1]])]
+    elif(otype=='c'):os=[np.array([(abs(ch)**0.75)*np.sign(ch) for ch in os[0]]),np.array([(abs(ch)**0.4)*np.sign(ch) for ch in os[1]])]
+    elif(otype=='s'):os=[np.array([(np.linalg.norm(sp)**0.75)*np.sign(sp[2]) for sp in os[0]]),np.array([(np.linalg.norm(sp)**0.4)*np.sign(sp[2]) for sp in os[1]])]
     # Imaginary
     bmp=[np.array([(b[n][0]+b[n][1])/2. for n in range(3)]) for b in bs]
     bv=[np.array([b[n][1]-b[n][0] for n in range(3)]) for b in bs]
     bii=np.array([bmp[nb]-(os[1][nb]/2.)*bv[nb] for nb in range(len(bs))]).transpose()
     biv=np.array([os[1][nb]*bv[nb] for nb in range(len(bs))]).transpose()
-    bspli=mlab.quiver3d(bii[0],bii[1],bii[2],biv[0],biv[1],biv[2],color=(0.4660,0.6740,0.1880),mode='cone',scale_factor=1.,resolution=res/2)
+    bspli=mlab.quiver3d(bii[0],bii[1],bii[2],biv[0],biv[1],biv[2],color=(0.4660,0.6740,0.1880),mode='cone',scale_factor=1.,resolution=res)
+    bspli.glyph.glyph_source.glyph_source.angle=20
+    bspli.glyph.glyph_source.glyph_source.height=0.6
     # Real
-    bsplr=[mlab.plot3d(b[0],b[1],b[2],colormap='coolwarm',tube_radius=0.05,tube_sides=res/2) for b in bs]
-    for n in range(len(bsplr)):
-        bsplr[n].module_manager.scalar_lut_manager.use_default_range=False
-        bsplr[n].module_manager.scalar_lut_manager.data_range=[-1.,1.]
-        bsplr[n].mlab_source.dataset.point_data.scalars=[os[0][n],os[0][n]]
+    [bv0,bv1,bv2]=np.array(bv).transpose()
+    [b00,b01,b02]=np.array([[b[n][0] for n in range(3)] for b in bs]).transpose()
+    bsplr=mlab.quiver3d(b00,b01,b02,bv0,bv1,bv2,colormap='coolwarm',mode='cylinder',scale_factor=1.,resolution=res)
+    bsplr.glyph.color_mode='color_by_scalar'
+    bsplr.module_manager.scalar_lut_manager.use_default_range=False
+    bsplr.module_manager.scalar_lut_manager.data_range=[-1.,1.]
+    bsplr.mlab_source.dataset.point_data.scalars=os[0]
+    bsplr.glyph.glyph_source.glyph_source.radius=0.05
+#    bsplr=[mlab.plot3d(b[0],b[1],b[2],colormap='coolwarm',tube_radius=0.05,tube_sides=res/2) for b in bs]
+#    for n in range(len(bsplr)):
+#        bsplr[n].module_manager.scalar_lut_manager.use_default_range=False
+#        bsplr[n].module_manager.scalar_lut_manager.data_range=[-1.,1.]
+#        bsplr[n].mlab_source.dataset.point_data.scalars=[os[0][n],os[0][n]]
 
 
-def plotlattice(rs,Nall,ltype,filetfig,otype='l',os=[[],[],[]],res=20,size=(5.,5.),setdpi=2000,to3d=True):
+def plotlattice(rs,Nall,ltype,filetfig,otype='l',os=[[],[],[]],res=50,size=(5.,5.),setdpi=2000,to3d=True):
     '''
     Plot the lattice
     '''
@@ -88,6 +101,7 @@ def plotlattice(rs,Nall,ltype,filetfig,otype='l',os=[[],[],[]],res=20,size=(5.,5
         f=mlab.gcf()
         f.scene._lift()
         arr=mlab.screenshot(mode='rgba',antialiased=True)
+#        mlab.show()
         mlab.clf()
         mlab.close()
     '''
